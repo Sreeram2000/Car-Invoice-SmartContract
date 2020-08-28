80bfe117b756a14aa74f47185b356e1ba1e2e0c4
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-28 14:06:31 +0530

    commit 1

---
+++ car-Invoice.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.4.18 <= 0.6.12;
+
+contract carInvoice {
+    
+    string customerName;
+    int     amtOfInvoice;
+    string  carMakeAndModel;
+    string  companyAndDealerName;
+    string  date;
+    string  addrsCustomer;
+    string  invoiceNo;
+    
+    //Initializing the variables
+    
+    function carInvoice(string newcustomerName,
+    int     newamtOfInvoice,
+    string  newcarMakeAndModel,
+    string  newcompanyAndDealerName,
+    string  newdate,
+    string  newaddrsCustomer,
+    string  newinvoiceNo) public {
+        customerName = newcustomerName;
+        amtOfInvoice = newamtOfInvoice;
+        carMakeAndModel = newcarMakeAndModel;
+        companyAndDealerName = newcompanyAndDealerName;
+        date = newdate;
+        addrsCustomer = newaddrsCustomer;
+        invoiceNo = newinvoiceNo;
+    }
+    
+    function getInvoice() public view returns(string,int,string,string,string,string,string) {
+        return (customerName,amtOfInvoice,carMakeAndModel,companyAndDealerName,date,addrsCustomer,invoiceNo) ;
+    }
+}
